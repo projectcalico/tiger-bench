83954425202b74032c2ba2d3199f95dd5f1de8a5
Lancelot Robson <lancelot.robson@gmail.com>
2025-03-28 17:00:14 +0000

    Add aws cli v2 to container to enable it to work with EKS clusters

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ex
-docker build -t quay.io/tigeradev/tiger-bench:v0.1.0 .
-docker run --rm --net=host \
+docker build -t quay.io/tigeradev/tiger-bench:latest .
+docker run -ti --rm --net=host \
 -v "${PWD}":/results \
 -v ${KUBECONFIG}:/kubeconfig \
 -v ${PWD}/testconfig.yaml:/testconfig.yaml \
 -v $HOME/.aws:/root/.aws \
 -e AWS_SECRET_ACCESS_KEY \
 -e AWS_ACCESS_KEY_ID \
+-e AWS_SESSION_TOKEN \
 -e LOG_LEVEL=INFO \
 -e WEBSERVER_IMAGE="quay.io/tigeradev/tiger-bench-nginx:main" \
 -e PERF_IMAGE="quay.io/tigeradev/tiger-bench-perf:main" \
-quay.io/tigeradev/tiger-bench:v0.1.0
+quay.io/tigeradev/tiger-bench:latest /bin/sh
