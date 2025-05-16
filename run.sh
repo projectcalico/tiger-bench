b60cabf4cacc93329a630be7aa97456f688d268b
Lancelot Robson <lancelot.robson@gmail.com>
2025-05-16 11:36:45 +0100

    Update versions for v0.3.0

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-docker build -t quay.io/tigeradev/tiger-bench:latest .
+docker build -t quay.io/tigeradev/tiger-bench:v0.3.0 .
 docker run --rm --net=host \
 -v "${PWD}":/results \
 -v ${KUBECONFIG}:/kubeconfig \
@@ -10,6 +10,7 @@ docker run --rm --net=host \
 -e AWS_ACCESS_KEY_ID \
 -e AWS_SESSION_TOKEN \
 -e LOG_LEVEL=INFO \
--e WEBSERVER_IMAGE="quay.io/tigeradev/tiger-bench-nginx:main" \
--e PERF_IMAGE="quay.io/tigeradev/tiger-bench-perf:main" \
-quay.io/tigeradev/tiger-bench:latest
+-e WEBSERVER_IMAGE="quay.io/tigeradev/tiger-bench-nginx:v0.3.0" \
+-e PERF_IMAGE="quay.io/tigeradev/tiger-bench-perf:v0.3.0" \
+-e TTFR_IMAGE="quay.io/tigeradev/tiger-bench-ttfr:v0.3.0" \
+quay.io/tigeradev/tiger-bench:v0.3.0
